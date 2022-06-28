05a7e92e4524a2cb00156b9c3cb7cf22d51291f3
lfbsdev <lfbsdev@gmail.com>
2022-06-28 16:43:06 -0300

    third commit

--- front-end/index.php
+++ front-end/index.php
@@ -10,7 +10,7 @@
         </form>
         <?php
 
-        $json = file_put_contents('http://o-zero/' . $_GET);
+        $json = file_get_contents('http://o-zero/' . $_GET['input']);
         $obj = json_decode($json);
 
         $res = $obj->output;
