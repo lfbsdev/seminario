527c7fa131d4631b53538337534ac61a7e0209b0
lfbsdev <lfbsdev@gmail.com>
2022-06-30 16:10:34 -0300

    commit 4

---
+++ a-quatro/api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class AQuatroTransformer(Resource):
+    def get(self, x):
+        x = x.replace('a', '4')
+        x = x.replace('A', '4')
+        return {'output': x}
+
+api.add_resource(AQuatroTransformer, '/<string:x>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
--- front-end/index.php
+++ front-end/index.php
@@ -3,19 +3,28 @@
         <title>Transforma String</title>
     </head>
 
-    <body>
+    <body style='text-align:center; padding:20px'>
         <h1>Olá, digite a string</h1>
         <form name="form" action="" method="get">
-            <input type="text" name="input" id="input-string" value="tosto">
+            <input type="text" name="input" id="input-string">
         </form>
         <?php
 
+        
         $json = file_get_contents('http://o-zero/' . $_GET['input']);
         $obj = json_decode($json);
 
         $res = $obj->output;
 
-        echo "<h1>Transformada fica $res</h1>"
+        echo "<h1>Trocando 'o' por '0' fica $res</h1>";
+
+        $json2 = file_get_contents('http://a-quatro/' . $res);
+        $obj2 = json_decode($json2);
+
+        $res2 = $obj2->output;
+
+        echo "<h1>Trocando 'a' por '4' fica $res2</h1>";;
+
         ?>
     </body>
 </html>
